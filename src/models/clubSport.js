63b1044a28f093e36c5cdf143d1faaa69c25abe7
Andres Mardones <agmardones@uc.cl>
2017-09-15 18:20:24 -0300

    Rename club_sport to clubSport js model

---
+++ src/models/clubSport.js
@@ -0,0 +1,8 @@
+module.exports = function definesports(sequelize, DataTypes) {
+  const clubSport = sequelize.define('clubSport', {});
+  clubSport.associate = function associate(models) {
+    clubSport.belongsTo(models.club);
+    clubSport.belongsTo(models.sport);
+  };
+  return clubSport;
+};
--- src/models/club_sport.js
+++
@@ -1,8 +0,0 @@
-module.exports = function definesports(sequelize, DataTypes) {
-  const club_sport = sequelize.define('club_sport', {});
-  club_sport.associate = function associate(models) {
-    club_sport.belongsTo(models.club);
-    club_sport.belongsTo(models.sport);
-  };
-  return club_sport;
-};
