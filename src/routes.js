db4cfacaabe5f8437c64efaa2a6217d52fc0bde1
Franco Mendez <fnmendez@uc.cl>
2017-09-20 18:31:37 -0300

    check login globally

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
 const KoaRouter = require('koa-router');
 
-const hello = require('./routes/hello');
 const welcome = require('./routes/welcome');
+const hello = require('./routes/hello');
+const checkLogin = require('./routes/checkLogin');
 const sports = require('./routes/sports');
 const clubs = require('./routes/clubs');
 const teams = require('./routes/teams');
@@ -9,8 +10,14 @@ const matches = require('./routes/matches');
 
 const router = new KoaRouter();
 
+// public
 router.use('/', welcome.routes());
 router.use('/hello', hello.routes());
+
+// check login
+router.use(checkLogin);
+
+// private
 router.use('/sports', sports.routes());
 router.use('/clubs', clubs.routes());
 router.use('/teams', teams.routes());
