caa0bf9f6b91353378dd1e1dbd758b9697d9da4f
Franco Mendez <fnmendez@uc.cl>
2017-09-29 10:20:05 -0300

    retrieve parsed date and added prototype methods

--- src/models/match.js
+++ src/models/match.js
@@ -1,7 +1,32 @@
+const parseDate = require('../routes/parseDate')
+
 module.exports = function definematch(sequelize, DataTypes) {
   const match = sequelize.define('match', {
-    date: DataTypes.DATE,
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  }, {
+    getterMethods: {
+      displayDate() {
+        return parseDate(this.date)
+      },
+    },
   })
+  match.prototype.howManyPlayers = async function howManyPlayers(ctx) {
+    const query = await ctx.orm.userMatch.findOne({
+      attributes: [[sequelize.fn('COUNT', sequelize.col('*')), 'amount']],
+      where: { matchId: this.id },
+    })
+    return query.dataValues.amount
+  }
+  match.prototype.howManyAdmins = async function howManyAdmins(ctx) {
+    const query = await ctx.orm.userMatch.findOne({
+      attributes: [[sequelize.fn('COUNT', sequelize.col('*')), 'amount']],
+      where: { matchId: this.id, admin: true },
+    })
+    return query.dataValues.amount
+  }
   match.associate = function associate(models) {
     match.belongsTo(models.club)
     match.belongsTo(models.sport)
