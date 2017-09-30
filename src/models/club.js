6174f0f07bde937ffdb8ba0a45f4408007043158
Andres Mardones <agmardones@uc.cl>
2017-09-30 19:55:56 -0300

    matches club info display

--- src/models/club.js
+++ src/models/club.js
@@ -16,6 +16,17 @@ module.exports = function defineclub(sequelize, DataTypes) {
       },
     },
   })
+
+  club.prototype.matchDisplay = function matchDisplay(sportid) {
+    // Instances refered with this might have clubsports loaded
+    var displayString = ""
+    this.clubSports.forEach( (clubSport) => {
+      if (clubSport.sportId === parseInt(sportid)) {
+        displayString = `${clubSport.priceDisplay()} por cada ${clubSport.timeUnit}`
+      }
+    })
+    return displayString
+  }
   club.associate = function associate(models) {
     club.hasMany(models.clubSport)
     club.hasMany(models.match)
