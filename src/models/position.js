28c5be2d418edf95d0dfd68dc9b03c85952c3999
Franco Mendez <fnmendez@uc.cl>
2017-09-30 16:07:03 -0300

    added model datatypes

--- src/models/position.js
+++ src/models/position.js
@@ -1,6 +1,15 @@
 module.exports = function defineposition(sequelize, DataTypes) {
   const position = sequelize.define('position', {
-    name: DataTypes.STRING,
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    horizontalAlignment: {
+      type: DataTypes.STRING,
+    },
+    verticalAlignment: {
+      type: DataTypes.STRING,
+    },
   })
   position.associate = function associate(models) {
     position.belongsTo(models.sport)
