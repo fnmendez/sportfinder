fab9745c5cf837365639cc99694a9e34fcb2ad68
Andres Mardones <agmardones@uc.cl>
2017-10-04 19:26:20 -0300

    Added isOver method on mathces

--- src/models/match.js
+++ src/models/match.js
@@ -51,6 +51,17 @@ module.exports = function definematch(sequelize, DataTypes) {
     }
     return false
   }
+
+  match.prototype.isOver = function isOver() {
+    const today = new Date()
+    const thisDate = new Date(this.date)
+    const diffInDays = (today - thisDate) / (1000 * 60 * 60 * 24)
+    if (diffInDays >= 1) {
+      return true
+    } else {
+      return false
+    }
+  }
   match.associate = function associate(models) {
     match.belongsTo(models.club)
     match.belongsTo(models.sport)
